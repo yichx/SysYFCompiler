8ab5df5fb79568268dbbcd1d8032a20e3d71bfb1
Kobe972 <xuyichang@mail.ustc.edu.cn>
2023-01-11 00:52:21 +0800

    modified stack alloc

--- SysYF_Backend_Student/src/Backend/StackAlloc.cpp
+++ SysYF_Backend_Student/src/Backend/StackAlloc.cpp
@@ -30,44 +30,48 @@ int CodeGen::stack_space_allocation(Function *fun)
     // std::vector<IR2asm::Regbase *> CodeGen::arg_on_stack
     arg_on_stack.clear();       // You need to maintain this information, the order is the same as parameter
 
-    int offset = (-used_reg.second.size()-1)*reg_size;
+    int offset = 0;
 
-    for(auto reg_interval_map = _reg_map->begin(); reg_interval_map != _reg_map->end(); reg_interval_map++) //溢出到栈的局部变量
+    if(have_func_call) //调用方保存区
     {
-      if(reg_interval_map->second->reg_num==-1)
-      {
-        stack_map.insert(std::make_pair(reg_interval_map->first,new IR2asm::Regbase(IR2asm::sp,offset)));
-        size+=reg_size;
-        offset-=reg_size;
-      }
+      size+=caller_saved_reg_num*reg_size;
+      offset+=caller_saved_reg_num*reg_size;
+    }
+    if(have_temp_reg)
+    {
+      size+=temp_reg_store_num*reg_size;
+      offset+=temp_reg_store_num*reg_size;
     }
     for(auto inst:fun->get_entry_block()->get_instructions()) //处理alloca
     {
       if(dynamic_cast<AllocaInst*>(inst)==nullptr) continue;
       if(dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->is_array_type())
       {
-        stack_map.insert(std::make_pair(inst,new IR2asm::Regbase(IR2asm::sp,offset-dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size())));
+        stack_map.insert(std::make_pair(inst,new IR2asm::Regbase(IR2asm::sp,offset)));
         size+=dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size();
         offset-=dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size();
       }
     }
-    if(have_temp_reg)
+    
+    for(auto reg_interval_map = _reg_map->begin(); reg_interval_map != _reg_map->end(); reg_interval_map++) //溢出到栈的局部变量
     {
-      size+=reg_size * temp_reg_store_num;
-      offset-=reg_size * temp_reg_store_num;
-    }
-    if(have_func_call)
-    {
-      if(caller_saved_reg_num>4)
+      if(reg_interval_map->second->reg_num==-1)
       {
-        offset-=(caller_saved_reg_num-5)*reg_size;
-        for(int i=0;i<caller_saved_reg_num-4;i++)
-        {
-          arg_on_stack.push_back(new IR2asm::Regbase(IR2asm::sp,offset));
-          offset+=reg_size;
-        }
+        stack_map.insert(std::make_pair(reg_interval_map->first,new IR2asm::Regbase(IR2asm::sp,offset)));
+        int _sizeof=reg_interval_map->first->get_type()->get_size();
+        if(_sizeof<4) _sizeof=4;
+        size+=_sizeof*reg_size;
+        offset+=_sizeof*reg_size;
       }
-      size+=caller_saved_reg_num*reg_size;
+    }
+    offset+=(used_reg.second.size()+1)*reg_size; //被调用方保存的寄存器
+    if(fun->get_num_of_args()>4) //函数参数
+    {
+      for(int i=0;i<fun->get_num_of_args()-4;i++)
+      {
+        arg_on_stack.push_back(new IR2asm::Regbase(IR2asm::sp, offset));
+        offset+=reg_size;
+      }
     }
 
     return size;

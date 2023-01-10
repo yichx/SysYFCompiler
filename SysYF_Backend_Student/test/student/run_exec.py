93006ae97f9801f66f1b04edf2ade469c1825d99
kobe972 <xuyichang@mail.ustc.edu.cn>
2023-01-10 09:21:36 +0000

    updated testing script

--- SysYF_Backend_Student/test/student/run_exec.py
+++ SysYF_Backend_Student/test/student/run_exec.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     os.chdir('../../build')
     levels=['Easy','Medium','Hard']
     datasets=['Test','Test_H']
+    print('Assemblying:')
     for dataset in datasets:
         for level in levels:
             if dataset=='Test_H':
@@ -21,6 +22,29 @@ if __name__ == '__main__':
             if not os.path.exists('../test/student/executable/'+dataset+'/'+level):
                 os.mkdir('../test/student/executable/'+dataset+'/'+level)
             for file in os.listdir('../test/student/'+dataset+'/'+level):
-                if file.endswith('.s'):
-                    subprocess.run(compile_cmd.format('../test/student/'+dataset+'/'+level+'/'+file,'../test/student/executable/'+dataset+'/'+level+'/'+file[:-2]+'s'),shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if file.endswith('.out'):
+                    asm_file = '../test/student/'+dataset+'/'+level+'/'+file[:-3]+'s'
+                    if not os.path.exists(asm_file):
+                        print('File '+dataset+'/'+level+'/'+file[:-3]+'.sy assembly failed!')
+                        continue
+                    subprocess.run(compile_cmd.format(asm_file,'../test/student/executable/'+dataset+'/'+level+'/'+file[:-4]),shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    print('File '+dataset+'/'+level+'/'+file[:-3]+'.sy assembly succeeded!')
+    print('Testing:')
+    test_cmd='cat {} | {}'
+    for dataset in datasets:
+        for level in levels:
+            if dataset=='Test_H':
+                level+='_H'
+            for file in os.listdir('../test/student/executable/'+dataset+'/'+level):
+                if os.path.exists('../test/student/'+dataset+'/'+level+'/'+file+'.in'):
+                    input_file='../test/student/'+dataset+'/'+level+'/'+file+'.in'
+                    result=subprocess.run(test_cmd.format(input_file, '../test/student/'+dataset+'/'+level+'/'+file),shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    with open('../test/student/'+dataset+'/'+level+'/'+file+'.out') as fp:
+                        content=fp.read()
+                    print(file,result,content)
+                else:
+                    result=subprocess.run(test_cmd.format('', '../test/student/'+dataset+'/'+level+'/'+file),shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    with open('../test/student/'+dataset+'/'+level+'/'+file+'.out') as fp:
+                        content=fp.read()
+                    print(file,result,content)
             

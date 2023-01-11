0ec9ccf3166ad0b63faa38ac2b1898e2bca6bbe1
Kobe972 <xuyichang@mail.ustc.edu.cn>
2023-01-11 13:14:00 +0800

    fixed stackalloc

--- SysYF_Backend_Student/src/Backend/StackAlloc.cpp
+++ SysYF_Backend_Student/src/Backend/StackAlloc.cpp
@@ -30,12 +30,10 @@ int CodeGen::stack_space_allocation(Function *fun)
     // std::vector<IR2asm::Regbase *> CodeGen::arg_on_stack
     arg_on_stack.clear();       // You need to maintain this information, the order is the same as parameter
 
-    int offset = 0;
-
-    if(have_func_call) //函数参数
+    if(have_func_call) //有函数调用，从fp开始从下网上
     {
         int base = (used_reg.second.size()+1)*reg_size; //被调用方保存的寄存器
-        if(fun->get_num_of_args()>4)
+        if(fun->get_num_of_args()>4) //函数参数
         {
           int arg_idx=0;
           for(auto arg:fun->get_args())
@@ -59,23 +57,30 @@ int CodeGen::stack_space_allocation(Function *fun)
           stack_map.insert(std::make_pair(reg_interval_map->first,new IR2asm::Regbase(IR2asm::frame_ptr,-size)));
         }
       }
-      for(auto inst:fun->get_entry_block()->get_instructions()) //处理alloca
+      for(auto bb:fun->get_basic_blocks()) //处理alloca
       {
-        if(dynamic_cast<AllocaInst*>(inst)==nullptr) continue;
-        if(dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->is_array_type())
+        for(auto inst:bb->get_instructions())
         {
-          size+=dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size();
-          stack_map.insert(std::make_pair(inst,new IR2asm::Regbase(IR2asm::frame_ptr,-size)));
+          if(dynamic_cast<AllocaInst*>(inst)==nullptr) continue;
+          if(dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->is_array_type())
+          {
+            size+=dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size();
+            stack_map.insert(std::make_pair(inst,new IR2asm::Regbase(IR2asm::frame_ptr,-size)));
+          }
         }
       }
-      if(have_temp_reg)
+      if(have_temp_reg) //临时寄存器保存区
       {
         size+=temp_reg_store_num*reg_size;
       }
+      size += caller_saved_reg_num * reg_size; //调用方保存区
     }
-    else
+    else //无函数调用，从sp开始从上往下
     {
-      size+=(temp_reg_store_num+1)*reg_size;
+      if(have_temp_reg) //临时寄存器保存区
+      {
+        size+=temp_reg_store_num*reg_size;
+      }
       for(auto reg_interval_map = _reg_map->begin(); reg_interval_map != _reg_map->end(); reg_interval_map++) //溢出到栈的局部变量
       {
         if(reg_interval_map->second->reg_num==-1)
@@ -86,13 +91,16 @@ int CodeGen::stack_space_allocation(Function *fun)
           size+=_sizeof;
         }
       }
-      for(auto inst:fun->get_entry_block()->get_instructions()) //处理alloca
+      for(auto bb:fun->get_basic_blocks()) //处理alloca
       {
-        if(dynamic_cast<AllocaInst*>(inst)==nullptr) continue;
-        if(dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->is_array_type())
+        for(auto inst:bb->get_instructions())
         {
-          stack_map.insert(std::make_pair(inst,new IR2asm::Regbase(IR2asm::sp,size)));
-          size+=dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size();
+          if(dynamic_cast<AllocaInst*>(inst)==nullptr) continue;
+          if(dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->is_array_type())
+          {
+            stack_map.insert(std::make_pair(inst,new IR2asm::Regbase(IR2asm::sp,size)));
+            size+=dynamic_cast<AllocaInst*>(inst)->get_alloca_type()->get_size();
+          }
         }
       }
       int base = (used_reg.second.size()+1)*reg_size+size; //被调用方保存的寄存器
